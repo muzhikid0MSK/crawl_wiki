02bae1d8c2f8eae8c27bec1c6094a9e5a01b6402
meishengke <631138727@qq.com>
2023-08-06 16:30:54 +0800

    add log print

--- Services/WikiService.py
+++ Services/WikiService.py
@@ -1,32 +1,29 @@
 import openpyxl
-import pyautogui
-import weasyprint
-from selenium import webdriver
+
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+
 import os
 from PIL import Image
 from concurrent.futures import ThreadPoolExecutor
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.expected_conditions import visibility_of_element_located
-from selenium.webdriver.support.wait import WebDriverWait
-
 import Services.Service
 from Utils.CharUtils import validatetitle
 import multiprocessing
-import time
-import win32api
-import win32con
+
 from threading import Lock
 
 from Utils import ThreadUtils
-
+import logging, coloredlogs
 
 class WikiService(Services.Service.Service):
 
     def __init__(self, excel_workbook: openpyxl.Workbook, column: int, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        logging.getLogger(__name__)
+        logging.basicConfig(format='%(asctime)s %(message)s')
+        coloredlogs.install()
+
         self.res = {}
         self.excel_worksheet = excel_workbook.active
         self.browsers = []
@@ -55,11 +52,12 @@ class WikiService(Services.Service.Service):
             cell = self.excel_worksheet.cell(row=r, column=active_col)
             self.__to_crawl.append(cell.value)
 
-        print("创建结果ROOT目录  -- 开始 --")
+
         self.result_root = os.path.join(self.ROOT, f"wiki-result-{self.person_name}", self.result_path)
+        logging.info(f"创建结果ROOT目录 {self.result_root} -- 开始 --")
         self.__mkdir(os.path.join(self.ROOT, f"wiki-result-{self.person_name}"))
         self.__mkdir(self.result_root)
-        print("创建结果ROOT目录  -- 结束 --")
+        logging.info(f"创建结果ROOT目录 {self.result_root}  -- 结束 --")
         print("")
         # for to_crawl in self.__to_crawl:
         #     to_crawl = validatetitle(to_crawl)
@@ -75,7 +73,7 @@ class WikiService(Services.Service.Service):
     def __handle_a_query(self, index_crawl):
         index = self._browser_index.increase_and_get()
         with self.browsers_locks[index]:
-            print(f"browser {index} is running")
+            logging.info(f"browser {index} is running")
             fullscreenshot_path = ""
             full_screen_img = None
             to_crawl = validatetitle(self.__to_crawl[index_crawl])
@@ -85,17 +83,19 @@ class WikiService(Services.Service.Service):
             self.res["urls"].append(url)
             img_open = None
             try:
-                print(f"{self.__to_crawl[index_crawl]} is running")
+                logging.info(f"---------------- 正在尝试在wiki上搜索{self.__to_crawl[index_crawl]}  ----------------")
                 self.browsers[index].get(url)
 
                 element = self.browsers[index].find_element("xpath", "//*[@*='infobox vcard']")
 
             except:
-                print(f"There is no {self.__to_crawl[index_crawl]} on wiki")
+                logging.error(f"---------------- There is no {self.__to_crawl[index_crawl]} on wiki ! ----------------")
             else:
+                logging.info(f"----------- 已搜索到{self.__to_crawl[index_crawl]} -----------")
                 self.__mkdir(os.path.join(self.result_root, to_crawl))
                 html_path = os.path.join(self.result_root, to_crawl,
                                          f"{validatetitle(self.browsers[index].title)}.mhtml")
+                logging.info(f"保存{self.__to_crawl[index_crawl]} mhtml成功")
                 self.__save_as_html(index, html_path)
                 width = self.browsers[index].execute_script("return document.documentElement.scrollWidth")
                 height = self.browsers[index].execute_script("return document.body.scrollHeight")
@@ -117,7 +117,7 @@ class WikiService(Services.Service.Service):
                 if full_screen_img.mode in ("RGBA", "P"):
                     full_screen_img = full_screen_img.convert("RGB")
                 full_screen_img.save(full_screen_pdf_path)
-
+                logging.info(f"保存{self.__to_crawl[index_crawl]} pdf 成功")
                 # html_path = os.path.join(self.result_root,  to_crawl,
                 #                          f"{validatetitle(self.browsers[i].title)}.html")
                 # with open(html_path, "w", encoding='utf-8') as f:
@@ -145,7 +145,7 @@ class WikiService(Services.Service.Service):
                 if img_open.mode in ("RGBA", "P"):
                     img_open = img_open.convert("RGB")
                 img_open.save(element_shot_path)
-
+                logging.info(f"保存{self.__to_crawl[index_crawl]} abstract 成功")
             finally:
                 if img_open is not None:
                     img_open.close()
@@ -153,9 +153,10 @@ class WikiService(Services.Service.Service):
                     full_screen_img.close()
 
     def __del__(self):
-        print("del")
+        logging.info(f"{self.result_path} is closing ")
         for browser in self.browsers:
             browser.close()
+        logging.info(f"{self.result_path} is closed ")
 
     def run(self, **params):
         super().run(**params)
